080154b861a9992717544634030d86a2abb98543
mshumko <msshumko@gmail.com>
2018-04-16 22:55:01 -0600

    Added the wavelet code to validation script.

--- validate_sampex.py
+++ validate_sampex.py
@@ -8,6 +8,7 @@ import os
 from datetime import datetime, timedelta
 
 import burst_parameter.burst_parameter as burst_parameter
+import wavelets.waveletAnalysis as waveletAnalysis
 
 import matplotlib.pyplot as plt
 
@@ -41,7 +42,7 @@ class ValidateBurstAlgorithm():
                 self.helt['dateTime'] = np.array([startTime+timedelta(seconds=dt) 
                                         for dt in rawData[:, i]])
             else:
-                self.helt[key] = rawData[:, i]#*10/15 # Convert to flux
+                self.helt[key] = rawData[:, i]/100#*10/15 # Convert to flux
         return
 
     def run_burst_param(self, ch='Rate5', thresh=10, N_WIDTH=0.1, A_WIDTH=0.5):
@@ -53,24 +54,37 @@ class ValidateBurstAlgorithm():
         self.detection = np.where(self.flag > thresh)[0]
         return
 
+    def run_wavelet_det(self, thresh=0.1):
+        self.w = waveletAnalysis.WaveletDetector(self.helt['Rate5'],
+                    self.helt['dateTime'], 0.1)
+        self.w.waveletTransform()    
+        self.w.waveletFilter(self.w.s0, 1)
+        self.w.degenerateInvWaveletTransform()
+        self.wDetection = np.where(self.w.dataFlt > thresh)[0]
+        return
+
 if __name__ == '__main__':
     fdir = '/home/mike/research/sampex/hilt'
     fname = 'hhrr1999229.txt'
     v = ValidateBurstAlgorithm(os.path.join(fdir, fname))
     v.run_burst_param()
+    v.run_wavelet_det()
 
     ### VISUALIZE DETECTIONS ###
     plt.plot(v.helt['dateTime'], v.helt['Rate5'], 'k')
     plt.scatter(v.helt['dateTime'][v.detection], 
                 v.helt['Rate5'][v.detection], 
                 marker='x', c='r', s=100)
+    plt.scatter(v.helt['dateTime'][v.wDetection], 
+                v.helt['Rate5'][v.wDetection], 
+                marker='x', c='g', s=100)
 
     plt.xlim(datetime(1999, 8, 17, 4, 11), datetime(1999, 8, 17, 4, 16))
     #plt.ylim(1, 10**4.5)
     plt.yscale('log')
     plt.xlabel('UTC')
     #plt.ylabel('SAMPEX SSD4 flux (rate 5)')
-    plt.ylabel('SAMPEX SSD4 counts/100 ms/100')
+    #plt.ylabel('SAMPEX SSD4 counts/100 ms/100')
     #plt.ylabel('SAMPEX SSD4 artificial')
     plt.title("O'brien burst parameter validation")
     plt.show()
