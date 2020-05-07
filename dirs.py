483feda96a4185f2a16afa7257d99e824b022f51
mike <msshumko@gmail.com>
2020-05-07 17:37:47 -0600

    Added the project directory and updated requirements.txt.

--- dirs.py
+++ dirs.py
@@ -1,2 +1,3 @@
 firebird_dir = lambda sc_id: (f'/home/mike/research/firebird/Datafiles'
                              f'/FU_{sc_id}/hires/level2')
+project_dir = '/home/mike/research/microburst_detection'
