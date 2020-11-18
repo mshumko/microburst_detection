2714cbbbeb78d29b61856f25faef4271ad27089c
Mykhaylo Shumko <msshumko@gmail.com>
2020-11-18 10:39:57 -0700

    Cleaned up the wavelet code quite a bit.

--- microburst_detection/wavelets/wavelet_test_scipt.py
+++ microburst_detection/wavelets/wavelet_test_scipt.py
@@ -1,37 +1,46 @@
-# wavelet test script
+"""
+Wavelet test script to make detections with the FIREBIRD data.
+"""
+import pathlib
+import string
+from datetime import datetime
+
 import spacepy.datamodel
 import pandas as pd
-# import spacepy.time
-import datetime
 import numpy as np
 import matplotlib.pylab as plt
-import matplotlib.gridspec as gridspec
-import pathlib
 
-import waveletAnalysis
-import dirs
+from microburst_detection.wavelets import wavelet_analysis
+from microburst_detection import config
 
 # Load example HiRes data
-hr_name = 'FU4_Hires_2019-09-27_L2.txt'
-sc_id = 4    
-hr_path = str(pathlib.Path(dirs.firebird_dir(sc_id), hr_name))
+sc_id = 4
+hr_date = datetime(2019, 9, 27) 
+search_str = f'FU{sc_id}*Hires*{hr_date.year}*{hr_date.month}*{hr_date.day}*L2*'
+hr_paths = list(pathlib.Path(config.FB_DIR, ).rglob(search_str))
+assert len(hr_paths) == 1, (f'A unique HiRes path not found.\n'
+                            f'hr_paths={hr_paths} search_str={search_str}')
+hr_path = str(hr_paths[0])
 hr = spacepy.datamodel.readJSONheadedASCII(hr_path)
+
 # Convert time array
 hr['Time'] = pd.to_datetime(hr['Time'])
 
-# # Filter the data by time to reduce computational time.
+# Filter the data by time to reduce computational time.
 if True:
-    startTime = datetime.datetime(2019, 9, 27, 18, 26, 30)
-    endTime = datetime.datetime(2019, 9, 27, 18, 29, 30)
+    startTime = datetime(2019, 9, 27, 18, 26, 30)
+    endTime = datetime(2019, 9, 27, 18, 29, 30)
     validInd = np.where((hr['Time'] > startTime) & (hr['Time'] < endTime))[0]
     assert len(validInd) > 0, "No data found in the time range specified!"
 else:
     validInd = np.arange(hr['Time'].shape[0])
 
-channel = 0
+channel=0
+count_thresh=0.1
+max_width = 1
 
 # Run the wavelet code
-waveDet = waveletAnalysis.WaveletDetector(
+waveDet = wavelet_analysis.WaveletDetector(
     hr['Col_counts'][validInd, channel],
     hr['Time'][validInd], 
     cadence=float(hr.attrs['CADENCE']), 
@@ -41,9 +50,9 @@ waveDet = waveletAnalysis.WaveletDetector(
     )
 
 waveDet.waveletTransform()
-waveDet.waveletFilter(waveDet.s0, 10.0)
+waveDet.waveletFilter(waveDet.s0, max_width)
 waveDet.degenerateInvWaveletTransform()
-waveDet.TestForMicrobursts(COUNT_THRESH=0.01)
+waveDet.TestForMicrobursts(COUNT_THRESH=count_thresh)
 waveDet.findMicroburstPeaks()
 
 fig, ax = plt.subplots(3, figsize=(8, 8), sharex=True)
@@ -51,23 +60,33 @@ fig, ax = plt.subplots(3, figsize=(8, 8), sharex=True)
 ## Plot raw count data, peaks, and wavelet-based significant (microburst) counts
 ax[0].plot(hr['Time'][validInd], 
             hr['Col_counts'][validInd, channel],
-             'b')
-ax[1].plot(hr['Time'][validInd], 
-            waveDet.dataFlt,
-             'b')
-ax[0].plot(hr['Time'][validInd[waveDet.indicies]],
-            hr['Col_counts'][validInd[waveDet.indicies], channel], 
-            'go', label='Microburst condition satisfied')
+             'k')
+# ax[0].plot(hr['Time'][validInd[waveDet.indicies]],
+#             hr['Col_counts'][validInd[waveDet.indicies], channel], 
+#             'go', label='Microburst condition satisfied')
 ax[0].plot(hr['Time'][validInd[waveDet.peaks]],
             hr['Col_counts'][validInd[waveDet.peaks], channel], 
             'r*', ls='None', markersize=10, label='microburst peak')
 
-ax[0].set(ylabel='Col counts [counts/bin]', title='Original data')
-ax[1].set(ylabel='Col counts [counts/bin]', title='Filtered data')
-ax[2].set(xlabel='Time', ylabel='period [s]', title='Wavelet power spectrum')
-ax[0].legend()
+ax[1].plot(hr['Time'][validInd], 
+            waveDet.dataFlt,
+             'k')
+ax[1].axhline(count_thresh, ls='--')
+
+ax[0].set(ylabel='counts [counts/bin]', 
+        title=f'FIREBIRD unit {sc_id} | collimated detector | {hr_date.date()}')
+ax[1].set(ylabel='counts [counts/bin]')
+ax[2].set(xlabel='Time', ylabel='period [s]')
+# ax[0].legend()
 
 # Plot wavelet power
 waveDet.plotPower(ax[-1])
+ax[-1].axhline(max_width, c='w', lw=3)
+
+subplot_titles = ['original data', 'filtered data', 'wavelet power spectrum']
+for i, (ax_i, title_i) in enumerate(zip(ax, subplot_titles)):
+    ax_i.text(0, 1, f'({string.ascii_lowercase[i]}) {title_i}', va='top',
+            transform=ax_i.transAxes, fontsize=15, weight='bold')
+
 plt.tight_layout()
 plt.show()
