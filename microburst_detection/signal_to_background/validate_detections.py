187bfc1891f70d042a3595993371e069e85fd449
Mykhaylo Shumko <msshumko@gmail.com>
2022-01-11 10:36:47 -0500

    Added the plot code.

--- microburst_detection/signal_to_background/validate_detections.py
+++ microburst_detection/signal_to_background/validate_detections.py
@@ -2,16 +2,22 @@
 import pathlib
 from datetime import datetime
 
+
 import progressbar
 import pandas as pd
+import numpy as np
 import spacepy
 import matplotlib.pyplot as plt
 
 from signal_to_background import config
 
+plot_window_s = 20
 sc_id = 4
 catalog_name = 'FU4_microburst_catalog_00.csv'
 catalog_path = pathlib.Path(config.PROJECT_DIR, 'data', catalog_name)
+
+save_dir = pathlib.Path(catalog_path.parents[0], 'validation_plots')
+save_dir.mkdir(parents=True, exist_ok=True)
 
 cat = pd.read_csv(catalog_path, index_col=0, parse_dates=True)
 cat = cat[(cat['time_gap'] == 1) | (cat['saturated'] == 1)]
@@ -36,4 +42,29 @@ for index, row in progressbar.progressbar(cat.iterrows(), max_value=cat.shape[0]
     if current_date.date() != index.date():
         hr = load_hr(index)
         current_date = index.date()
-    pass
+    dt = pd.Timedelta(second=plot_window_s/2)
+    time_range = (index-dt, index+dt)
+
+    _, ax = plt.subplots()
+
+    idt = np.where(
+        (hr['Time'] > time_range[0]) &
+        (hr['Time'] < time_range[1])
+        )[0]
+    idt_peak = np.where(hr['Time'] == index)[0]
+    ax.plot(hr['Time'][idt], hr['Col_counts'][idt, 0], c='k')
+    ax.scatter(hr['Time'][idt_peak], hr['Col_counts'][idt_peak, 0], m='*', s=200, c='r')
+
+    ax.set(
+        xlim=time_range, xlabel='Time', 
+        ylabel=f'Counts/{float(hr.attrs["CADENCE"])}',
+        title=index.strftime("%Y-%m-%d %H:%M:%S microburst validation")
+        )
+    ax.text(0.8, 1, f'time_gap={row["time_gap"]}\nsaturation={row["saturation"]}', 
+        va='top', transform=ax.transAxis)
+
+    plt.tight_layout()
+    save_name = index.strftime("%Y%m%d_%H%M%S_microburst.png")
+    save_path = pathlib.Path(save_dir, save_name)
+    plt.savefig(save_path)
+    del(ax)
